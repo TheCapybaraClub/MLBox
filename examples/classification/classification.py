2167318b2081a187f71bb4e4bf0339c3403e7c1c
Adrian Boeh <adrian.boeh@schneider-electric.com>
2019-09-04 14:27:11 -0400

    added save to both classification examples

--- examples/classification/classification.py
+++ examples/classification/classification.py
@@ -67,3 +67,27 @@ best = opt.optimise(space, data, 15)
 # Make prediction and save the results in save folder.
 prd = Predictor()
 prd.fit_predict(best, data)
+
+try:
+    fhand = open(self.to_path + '/scoring_best.obj', 'wb')
+    pickle.dump(best, fhand)
+    fhand.close()
+    print("Dumpped scoring_best.obj into directory")
+except:
+    print("unable to dump scoring_best.obj into directory")
+
+try:
+    fhand = open(self.to_path + '/scoring_pipeline.obj', 'wb')
+    pickle.dump(prd.scoring_pipeline, fhand)
+    fhand.close()
+    print("Dumpped scoring_pipeline.obj into directory")
+except:
+    print("unable to dump scoring_pipeline.obj into directory")
+
+try:
+    fhand = open(self.to_path + '/scoring_encoder.obj', 'wb')
+    pickle.dump(prd.scoring_encoder, fhand)
+    fhand.close()
+    print("Dumpped scoring_encoder.obj into directory")
+except:
+    print("unable to dump scoring_encoder.obj into directory")
